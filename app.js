ae775c721e4d2f2db630dc10d040b3de85376607
cmn <mokayaclinton34@gmail.com>
2022-08-21 23:07:47 +0300

    Write function to play one round of the game

--- app.js
+++ app.js
@@ -3,5 +3,31 @@ function getComputerChoice()
 {
     return (choice[~~(Math.random() * 3)]);
 }
-console.log (getComputerChoice());
 
+let playerSelection = "PAPER";
+let computerSelection;
+function playRound(playerSelection, computerSelection)
+{
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = getComputerChoice().toLowerCase();
+    if (playerSelection == computerSelection)
+    {
+        return ("You tie!");
+    }
+    else if (
+    (computerSelection == "rock" && playerSelection == "scissors") ||
+    (computerSelection == "paper" && playerSelection == "rock") ||
+    (computerSelection == "scissors" && playerSelection == "paper")
+    )
+    {
+        return (`You Lose! ${computerSelection} beats ${playerSelection}`);
+    }
+    else if (
+    (playerSelection == "rock" && computerSelection == "scissors") ||
+    (playerSelection == "paper" && computerSelection == "rock") ||
+    (playerSelection == "scissors" && computerSelection == "paper")
+    )
+    {
+        return (`You win! ${playerSelection} beats ${computerSelection}`);
+    }
+}
