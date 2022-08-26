8d05840f3f261b0971ead67d80af59f2964d7f40
cmn <mokayaclinton34@gmail.com>
2022-08-26 15:14:53 +0300

    Write function to play game in rounds, display round results, eventual winner

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ function getComputerChoice()
     return (choice[~~(Math.random() * 3)]);
 }
 
+function getPlayerChoice()
+{
+    let playerChoice = prompt("Rock, Paper or Scissors? ");
+    let lowerCaseInput = playerChoice.trim().toLowerCase();
+    return (lowerCaseInput);
+}
 let playerSelection = "PAPER";
 let computerSelection;
 function playRound(playerSelection, computerSelection)
@@ -31,3 +37,45 @@ function playRound(playerSelection, computerSelection)
         return (`You win! ${playerSelection} beats ${computerSelection}`);
     }
 }
+
+function game()
+{
+    let roundResult;
+    let playerPoints = 0;
+    let compPoints = 0;
+    let playerTurn = "";
+    let compTurn ="";
+    let rounds;
+    let i;
+
+    rounds = prompt("How many rounds do you want to play? ");
+    for (i = 0; i < rounds; i++)
+    {
+        compTurn = getComputerChoice();
+        playerTurn = getPlayerChoice();
+        roundResult = playRound(playerTurn, compTurn);
+        if (roundResult.includes("You win"))
+        {
+            playerPoints++;
+        }
+        else if (roundResult.includes("You Lose"))
+        {
+            compPoints++;
+        }
+        console.log(roundResult);
+    }
+
+    if (playerPoints > compPoints)
+    {
+        console.log("You won the game " + playerPoints + " to " + compPoints + " !");
+    }
+    else if (compPoints > playerPoints)
+    {
+        console.log("You lost the game " + compPoints + " to " + playerPoints + " !");
+    }
+    else
+    {
+        console.log("You tied at " + playerPoints + " points!");
+    }
+}
+game();
